7235f5ed2c0a3f8db9215d6143e6689fb5d2e77e
erickgcia <erick17a@hotmail.com>
2024-03-26 13:54:36 -0600

    Fetch temporal API with non official products for the project.

--- 03-react-III/src/components/Products.jsx
+++ 03-react-III/src/components/Products.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export const Products = () => {
-  const url = 'https://fakestoreapi.com/products'
-  fetch(url)
-    .then(res => res.json())
-    .then(json => console.log(json))
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products')
+        setProducts(response.data)
+        setLoading(false)
+      } catch(err) {
+        console.error(err)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   return (
-    <article>
+    <article className="products">
+      {loading ?
+        <div className='loading'>
+          <h1>Loading...</h1>
+        </div> :
+        products.map((product, index) => (
+          <section key={index} className="products__card">
+            <img className="products__card-img" src={product.image} alt={product.title} />
+            <div className='products__wrapper'>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <span>{`$ ${product.price.toFixed(2)}`}</span>
+            </div>
+          </section>
+        ))
+      }
     </article>
   )
 }
