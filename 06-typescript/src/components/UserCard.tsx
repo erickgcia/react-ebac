0bdcc9a6641875d9d4f52cac0168166acbb2d13f
erickgcia <erick17a@hotmail.com>
2024-05-27 16:24:23 -0600

    add: UserCard testing

--- 06-typescript/src/components/UserCard.tsx
+++ 06-typescript/src/components/UserCard.tsx
@@ -13,11 +13,16 @@ export type UserCardProps = {
 function UserCard({ user, getDepartmentClass }: UserCardProps) {
   return (
     <div
+      role="contentinfo"
       className={`border-2 rounded-lg p-4 border-white
       ${getDepartmentClass(user.company.department)}
     `}
     >
-      <img className="w-full p-8" src={user.image} alt="user image" />
+      <img
+        className="w-full p-8"
+        src={user.image}
+        alt={`${user.username} image`}
+      />
       <p className="text-lg text-white font-bold">{user.username}</p>
       <p className="text-base font-medium text-slate-600">
         {user.company.department}
---
+++ 06-typescript/src/components/__tests__/UserCard.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen } from '@testing-library/react'
+import UserCard, { type UserCardProps } from '../UserCard.tsx'
+
+describe('UserCard component', () => {
+  it('Test that user card renders properly', () => {
+    render(
+      <UserCard
+        user={{
+          username: '',
+          image: '',
+          email: '',
+          company: { department: '' },
+        }}
+        getDepartmentClass={() => ''}
+      />
+    )
+
+    const userCardElement = screen.getByRole('contentinfo')
+    expect(userCardElement).toBeInTheDocument()
+  })
+
+  it('Test that all the user information is render correctly', () => {
+    const mockUser: UserCardProps['user'] = {
+      username: 'emilys',
+      image: 'https://dummyjson.com/icon/emilys/128',
+      email: 'emily.johnson@x.dummyjson.com',
+      company: { department: 'Engineering' },
+    }
+
+    render(<UserCard user={mockUser} getDepartmentClass={() => ''} />)
+
+    const userImg = screen.getByAltText(`${mockUser.username} image`)
+    expect(userImg).toBeInTheDocument()
+    expect(userImg).toHaveAttribute('src', mockUser.image)
+
+    const usernameTxt = screen.getByText(mockUser.username)
+    expect(usernameTxt).toBeInTheDocument()
+
+    const userDpto = screen.getByText(mockUser.company.department)
+    expect(userDpto).toBeInTheDocument()
+
+    const userEmail = screen.getByText(mockUser.email)
+    expect(userEmail).toBeInTheDocument()
+  })
+
+  it('Test that getDepartmentClass function is displayed with the corresponding color', () => {
+    const mockUser: UserCardProps['user'] = {
+      username: 'emilys',
+      image: 'https://dummyjson.com/icon/emilys/128',
+      email: 'emily.johnson@x.dummyjson.com',
+      company: { department: 'Engineering' },
+    }
+
+    const getDepartmentClassMock = jest.fn().mockReturnValue('bg-neutral-400')
+
+    render(
+      <UserCard user={mockUser} getDepartmentClass={getDepartmentClassMock} />
+    )
+
+    const userCardElement = screen.getByRole('contentinfo')
+    expect(userCardElement).toBeInTheDocument()
+    expect(getDepartmentClassMock).toHaveBeenCalledWith(
+      mockUser.company.department
+    )
+  })
+})
